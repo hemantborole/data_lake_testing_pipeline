5edc69cfc35775aa258aca6bb4aaaec46fcd4a82
Borole <boroleh@f8ffc257df04.ant.amazon.com>
2020-11-23 23:56:09 -0800

    Cleaned up testing flow to update timestamps

--- src/pickTest.py
+++ src/pickTest.py
@@ -2,48 +2,47 @@ import json
 import boto3
 import datetime
 
+### Test picker to pick an active test, identify the ARN/test params and hand
+### it to step function for execution.
+
 session = boto3.Session(region_name = 'us-west-2')
 s3 = session.resource("s3")
 ddb = session.client("dynamodb")
 
+CONFIG_TABLE = 'datalake-test-config'
+        
 def lambda_handler(event, context):
-    ## full scan ddb
-    #tests = ddb.scan(TableName='datalake-test-config')
     
     testid = event.get('test_id')
-    if testid != None:
-        item_old = ddb.get_item(
-            TableName='datalake-test-config',
-            Key = {'test_id' : {'S': testid},
-            'active':{'N':'1'}})
-        item_old['Item']['endedAt'] = {'S':str(datetime.datetime.now())}
-        ddb.put_item(TableName='datalake-test-config', Item=item_old['Item'])
-        item_new = ddb.get_item(
-            TableName='datalake-test-config',
-            Key = {'test_id' : {'S': str(int(testid)+1)},
-            'active':{'N':'1'}})
-        if item_new and item_new.get('Item'):
-            item = item_new.get('Item')
-        else:
-            item = None
-    else:
-        ## pick the first item from the table
-        first_item = ddb.get_item(
-            TableName='datalake-test-config',
-            Key = {'test_id' : {'S': '1'},
-            'active':{'N':'1'}})
-        item = first_item.get('Item')
-
-
-    if None == item:
+    print("testid:" + str(testid))
+    if None == testid:
+        testid = '0'
+    
+    first_item = ddb.scan( TableName=CONFIG_TABLE,
+            ScanFilter = {
+                'test_id' : {'AttributeValueList':[{'S':testid}],
+                'ComparisonOperator':'EQ'}
+            }
+        )
+    
+    ### If no test is found, the testing ends there.
+    if None == first_item or 0 >= len(first_item.get('Items')):
         return { 'job': 'null', 'test_id': testid}
-    else:
+    
+    item = first_item.get('Items')[0]
+    is_active = item.get('active').get('N')
+    
+    if( "1" == str(is_active)):
         item['startedAt'] = {'S':str(datetime.datetime.now())}
-        testid = item.get('test_id').get('S')
-        
-        ddb.put_item(TableName='datalake-test-config', Item=item)
-        return { 'job' : item.get('job').get('S'),
+        ddb.put_item(TableName=CONFIG_TABLE, Item=item)
+        rt =  { 'job' : item.get('job').get('S'),
             'TaskExecution': item.get('job_arn').get('S').split("/")[-1],
             'TaskValidation': item.get('validation_lambda_arn').get('S').split(":")[-1],
-            'test_id': testid
+            'test_id': testid,
+            'params': item.get('job_input').get('M')
         }
+    else:
+        ## If the test is not active, keep moving to the next test.
+        rt =  { 'job' : 'skip', 'test_id' : testid }
+    
+    return rt
